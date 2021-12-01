66b832538aba653f63cd4564b9bd2e480a60afb2
huseyin cinibulak <huseyincnblk@gmail.com>
2021-12-01 12:37:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace patika_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("isminizi girin");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyisim giriniz");
+            string surname = Console.ReadLine();
+            Console.WriteLine("merhaba " + name + " " + surname);
+        }
+    }
+}
